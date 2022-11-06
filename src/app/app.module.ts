1f61b71549698de8e4e71e9a93181a61f9bad27a
Rafael Neves Gomila <rafael.gomila@hotmail.com>
2022-11-06 18:22:33 -0300

    Component presentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    PresentationComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
